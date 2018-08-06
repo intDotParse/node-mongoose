b2e572a325383ba0a5a2960e64c210f225310c26
Artemio Cabrillos JR <act.acabrillos@gmail.com>
2018-08-06 20:28:42 +0800

    added tokens but with error duplication of email still accepted

--- server/models/user.js
+++ server/models/user.js
@@ -1,20 +1,38 @@
 const mongoose = require('mongoose');
-let users = mongoose.model('User', {
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
+
+let UserSchema = new mongoose.Schema({
     email: {
         type: String,
-        required: true
-    },
-    username: {
-        type: String,
-        required: true
+        required: true,
+        trim: true,
+        minlength: 10,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: 'email is not valid!'
+        }
     },
     password: {
         type: String,
-        required: true
+        required: true,
+        minlength: 6
     },
+    tokens: [{
+        access: {
+            type: String,
+            requred: true
+        },
+        token: {
+            type: String,
+            requred: true
+        }
+    }],
     status: {
         type: String,
-        default: 'active'
+        default: 'active',
     },
     createdAt: {
         type: Date,
@@ -22,6 +40,29 @@ let users = mongoose.model('User', {
     }
 });
 
+UserSchema.methods.toJSON = function () {
+    let user = this;
+    let userObject = user.toObject();
+
+    return _.pick(userObject, ['_id', 'email']);
+}
+
+UserSchema.methods.generateAuthToken = function () {
+    let user = this;
+    let access = 'auth';
+    let token = jwt.sign({ _id: user._id.toHexString(), access }, 'artemio').toString();
+
+    user.tokens.push({
+        access,
+        token
+    });
+    return user.save().then(() => {
+        return token;
+    });
+}
+
+let Users = mongoose.model('Users', UserSchema);
+
 module.exports = {
-    users
+    Users
 }
--- server/server.js
+++ server/server.js
@@ -1,39 +1,48 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const _ = require('lodash');
 
 const { mongoose } = require('./db/mongoose');
-const { users } = require('./models/user');
+const { Users } = require('./models/user');
 
 var app = express();
 
 app.use(bodyParser.json());
 
-app.post('/adduser', (request, response) => {
-    let user = new users({
-        email: request.body.email,
-        username: request.body.username,
-        password: request.body.password,
-        status: 'active',
-        createdAt: new Date()
-    }).save().then((doc) => {
-        response.send(doc);
-    }, (err) => {
-        response.status(400).send(err);
+app.get('/user', (request, response) => {
+    Users.find().then((userdoc) => {
+        response.send(userdoc);
+    }, (e) => {
+        response.status(400).send(e);
     });
 });
 
-app.get('/', (request, response) => {
-    users.find().then((user) => {
-        response.send({
-            user
-        });
-    },(err)=>{
-        response.status(400).send(err);
+app.post('/user', (request, response) => {
+    let body = _.pick(request.body, ['email', 'password']);
+    let user = new Users(body);
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        response.header('x-auth', token).send(user);
+    }).catch((e) => {
+        response.status(400).send(e);
     });
-
 });
 
-app.listen(3000, () => {
-    console.log(`Server running @ port 3000! `);
-});
+    // app.get('/', (request, response) => {
+    //     users.find().then((user) => {
+    //         response.send({
+    //             user
+    //         });
+    //     },(err)=>{
+    //         response.status(400).send(err);
+    //     });
+
+    // });
+
+
+
+    app.listen(3000, () => {
+        console.log(`Server running @ port 3000! `);
+    });
 
